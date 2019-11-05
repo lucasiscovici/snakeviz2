e4639faa4a0b12c2158df854fd149fc3215153e6
lucasiscovici <luluisco@yahoo.fr>
2019-11-05 15:09:06 +0100

    Update cli.py

--- snakeviz/cli.py
+++ snakeviz/cli.py
@@ -124,7 +124,7 @@ def main(argv=None):
                                      httputil.HTTPServerRequest(uri="",connection=rien()))
         from .main import VizHandler
         html=VizHandler.get(w,filename_)
-        html=w.htht
+        html=w.httt
         file= open(f,"w")
         #write then close file
         file.write(html)
